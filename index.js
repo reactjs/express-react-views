30a4b294b3d748d32019d29b7ffb4eaaaf01c4c0
Oleg Zemelia <oleg.zemelia@io1.co>
2015-01-14 15:30:36 +0200

    Fix uglifyify issue. Add caching ability(in memory for now).

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ var React = require('react'),
   reactify    = require('reactify'),
   nodeJSX = require('node-jsx'),
   Stream = require('stream'),
-  reactString = new Stream;
+  reactString = new Stream,
+  uglifyify = require('uglifyify'),
+  crypto = require('crypto'),
+  cacheHash = {};
 
 reactString.writable = true;
 
@@ -31,44 +34,56 @@ function createEngine(engineOptions) {
   nodeJSX.install(engineOptions.jsx);
 
   function renderFile(filename, options, cb) {
-    try {
-      var script = markup = '';
-      var component = require(filename);
-      component = React.createFactory(component);
-      markup = engineOptions.doctype;
-      markup += '<html><head></head><body id="body">';
-      markup += React.renderToString(component(options));
+    // Create hash based on filename + options unique pair.
+    var shasum = crypto.createHash('sha1');
+    var optionsString = JSON.stringify(options);
+    shasum.update(filename + optionsString);
+    var hash = shasum.digest('hex');
 
-      var b = browserify();
-      b.require(filename, {expose: 'MyApp'})
-        .transform(reactify)
-        .transform({global: true}, literalify.configure({react: 'window.React'}))
-        .transform({global: true}, 'uglifyify')
-        .bundle().pipe(reactString);
+    if (!cacheHash[hash]) {
+      try {
+        var script = markup = '';
+        var component = require(filename);
+        component = React.createFactory(component);
+        markup = engineOptions.doctype;
+        markup += '<html><head></head><body id="body">';
+        markup += React.renderToString(component(options));
+        var b = browserify();
+        b.require(filename, {expose: 'MyApp'})
+          .transform(reactify)
+          .transform({global: true}, literalify.configure({react: 'window.React'}))
+          .transform({global: true}, uglifyify)
+          .bundle().pipe(reactString);
 
-      reactString.write = function(buf) {
-        script += buf.toString();
-      };
-      reactString.end = function(buf) {
-        if (arguments.length) {
-          reactString.write(buf);
+        reactString.write = function (buf) {
+          script += buf.toString();
+        };
+        reactString.end = function (buf) {
+          if (arguments.length) {
+            reactString.write(buf);
+          }
+          if (script.length) {
+            markup += '<script src=//fb.me/react-0.12.2.min.js></script>';
+            markup += '<script>' + script +
+            'var MyApp = React.createFactory(require("MyApp"));' +
+            'React.render(MyApp(' + safeStringify(options) + '), document.getElementById("body"))' +
+            '</script>';
+            markup += '</body></html>'
+          }
+          reactString.writable = false;
+
+          // Add to cache.
+          cacheHash[hash] = markup;
+          cb(null, markup);
+        };
+        reactString.destroy = function () {
+          reactString.writable = false;
         }
-        if (script.length) {
-          markup += '<script src=//fb.me/react-0.12.2.min.js></script>';
-          markup += '<script>' + script +
-          'var MyApp = React.createFactory(require("MyApp"));' +
-          'React.render(MyApp(' + safeStringify(options) + '), document.getElementById("body"))' +
-          '</script>';
-          markup += '</body></html>'
-        }
-        reactString.writable = false;
-        cb(null, markup);
-      };
-      reactString.destroy = function() {
-        reactString.writable = false;
+      } catch (e) {
+        return cb(e);
       }
-    } catch (e) {
-      return cb(e);
+    } else {
+      cb(null, cacheHash[hash]);
     }
   }
   return renderFile;
